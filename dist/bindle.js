9fd2e9fe7ae8626a2e68b2a19023ddcd2a86a888
Isaac Martin <isaac@weareplayground.co>
2019-09-02 10:46:31 -0400

    platform: webpack ts

---
+++ dist/bindle.js
@@ -0,0 +1,672 @@
+!(function(e, t) {
+	if ('object' === typeof exports && 'object' === typeof module) {
+		module.exports = t()
+	} else if ('function' === typeof define && define.amd) {
+		define([], t)
+	} else {
+		const r = t()
+		for (const n in r) {
+			;('object' === typeof exports ? exports : e)[n] = r[n]
+		}
+	}
+})(window, function() {
+	return (function(e) {
+		const t = {}
+		function r(n) {
+			if (t[n]) {
+				return t[n].exports
+			}
+			const o = (t[n] = { i: n, l: !1, exports: {} })
+			return e[n].call(o.exports, o, o.exports, r), (o.l = !0), o.exports
+		}
+		return (
+			(r.m = e),
+			(r.c = t),
+			(r.d = function(e, t, n) {
+				r.o(e, t) ||
+					Object.defineProperty(e, t, { enumerable: !0, get: n })
+			}),
+			(r.r = function(e) {
+				'undefined' !== typeof Symbol &&
+					Symbol.toStringTag &&
+					Object.defineProperty(e, Symbol.toStringTag, {
+						value: 'Module'
+					}),
+					Object.defineProperty(e, '__esModule', { value: !0 })
+			}),
+			(r.t = function(e, t) {
+				if ((1 & t && (e = r(e)), 8 & t)) {
+					return e
+				}
+				if (4 & t && 'object' === typeof e && e && e.__esModule) {
+					return e
+				}
+				const n = Object.create(null)
+				if (
+					(r.r(n),
+					Object.defineProperty(n, 'default', {
+						enumerable: !0,
+						value: e
+					}),
+					2 & t && 'string' !== typeof e)
+				) {
+					for (const o in e) {
+						r.d(
+							n,
+							o,
+							function(t) {
+								return e[t]
+							}.bind(null, o)
+						)
+					}
+				}
+				return n
+			}),
+			(r.n = function(e) {
+				const t =
+					e && e.__esModule
+						? function() {
+								return e.default
+						  }
+						: function() {
+								return e
+						  }
+				return r.d(t, 'a', t), t
+			}),
+			(r.o = function(e, t) {
+				return Object.prototype.hasOwnProperty.call(e, t)
+			}),
+			(r.p = ''),
+			r((r.s = 3))
+		)
+	})([
+		function(e, t, r) {
+			'use strict'
+			e.exports = r(1)
+		},
+		function(e, t, r) {
+			'use strict'
+			/** @license React v16.9.0
+			 * react.production.min.js
+			 *
+			 * Copyright (c) Facebook, Inc. and its affiliates.
+			 *
+			 * This source code is licensed under the MIT license found in the
+			 * LICENSE file in the root directory of this source tree.
+			 */ const n = r(2),
+				o = 'function' === typeof Symbol && Symbol.for,
+				u = o ? Symbol.for('react.element') : 60103,
+				c = o ? Symbol.for('react.portal') : 60106,
+				f = o ? Symbol.for('react.fragment') : 60107,
+				l = o ? Symbol.for('react.strict_mode') : 60108,
+				i = o ? Symbol.for('react.profiler') : 60114,
+				a = o ? Symbol.for('react.provider') : 60109,
+				s = o ? Symbol.for('react.context') : 60110,
+				p = o ? Symbol.for('react.forward_ref') : 60112,
+				y = o ? Symbol.for('react.suspense') : 60113,
+				d = o ? Symbol.for('react.suspense_list') : 60120,
+				v = o ? Symbol.for('react.memo') : 60115,
+				h = o ? Symbol.for('react.lazy') : 60116
+			o && Symbol.for('react.fundamental'),
+				o && Symbol.for('react.responder')
+			const m = 'function' === typeof Symbol && Symbol.iterator
+			function b(e) {
+				for (
+					var t = e.message,
+						r =
+							'https://reactjs.org/docs/error-decoder.html?invariant=' +
+							t,
+						n = 1;
+					n < arguments.length;
+					n++
+				) {
+					r += '&args[]=' + encodeURIComponent(arguments[n])
+				}
+				return (
+					(e.message =
+						'Minified React error #' +
+						t +
+						'; visit ' +
+						r +
+						' for the full message or use the non-minified dev environment for full errors and additional helpful warnings. '),
+					e
+				)
+			}
+			const g = {
+					isMounted() {
+						return !1
+					},
+					enqueueForceUpdate() {},
+					enqueueReplaceState() {},
+					enqueueSetState() {}
+				},
+				S = {}
+			function j(e, t, r) {
+				;(this.props = e),
+					(this.context = t),
+					(this.refs = S),
+					(this.updater = r || g)
+			}
+			function _() {}
+			function w(e, t, r) {
+				;(this.props = e),
+					(this.context = t),
+					(this.refs = S),
+					(this.updater = r || g)
+			}
+			;(j.prototype.isReactComponent = {}),
+				(j.prototype.setState = function(e, t) {
+					if (
+						'object' !== typeof e &&
+						'function' !== typeof e &&
+						null != e
+					) {
+						throw b(Error(85))
+					}
+					this.updater.enqueueSetState(this, e, t, 'setState')
+				}),
+				(j.prototype.forceUpdate = function(e) {
+					this.updater.enqueueForceUpdate(this, e, 'forceUpdate')
+				}),
+				(_.prototype = j.prototype)
+			const O = (w.prototype = new _())
+			;(O.constructor = w),
+				n(O, j.prototype),
+				(O.isPureReactComponent = !0)
+			const x = { current: null },
+				k = { suspense: null },
+				E = { current: null },
+				P = Object.prototype.hasOwnProperty,
+				C = { key: !0, ref: !0, __self: !0, __source: !0 }
+			function $(e, t, r) {
+				let n = void 0,
+					o = {},
+					c = null,
+					f = null
+				if (null != t) {
+					for (n in (void 0 !== t.ref && (f = t.ref),
+					void 0 !== t.key && (c = String(t.key)),
+					t)) {
+						P.call(t, n) && !C.hasOwnProperty(n) && (o[n] = t[n])
+					}
+				}
+				let l = arguments.length - 2
+				if (1 === l) {
+					o.children = r
+				} else if (1 < l) {
+					for (var i = Array(l), a = 0; a < l; a++) {
+						i[a] = arguments[a + 2]
+					}
+					o.children = i
+				}
+				if (e && e.defaultProps) {
+					for (n in (l = e.defaultProps)) {
+						void 0 === o[n] && (o[n] = l[n])
+					}
+				}
+				return {
+					$$typeof: u,
+					type: e,
+					key: c,
+					ref: f,
+					props: o,
+					_owner: E.current
+				}
+			}
+			function R(e) {
+				return 'object' === typeof e && null !== e && e.$$typeof === u
+			}
+			const V = /\/+/g,
+				M = []
+			function z(e, t, r, n) {
+				if (M.length) {
+					const o = M.pop()
+					return (
+						(o.result = e),
+						(o.keyPrefix = t),
+						(o.func = r),
+						(o.context = n),
+						(o.count = 0),
+						o
+					)
+				}
+				return {
+					result: e,
+					keyPrefix: t,
+					func: r,
+					context: n,
+					count: 0
+				}
+			}
+			function A(e) {
+				;(e.result = null),
+					(e.keyPrefix = null),
+					(e.func = null),
+					(e.context = null),
+					(e.count = 0),
+					10 > M.length && M.push(e)
+			}
+			function I(e, t, r) {
+				return null == e
+					? 0
+					: (function e(t, r, n, o) {
+							let f = typeof t
+							;('undefined' !== f && 'boolean' !== f) ||
+								(t = null)
+							let l = !1
+							if (null === t) {
+								l = !0
+							} else {
+								switch (f) {
+									case 'string':
+									case 'number':
+										l = !0
+										break
+									case 'object':
+										switch (t.$$typeof) {
+											case u:
+											case c:
+												l = !0
+										}
+								}
+							}
+							if (l) {
+								return n(o, t, '' === r ? '.' + q(t, 0) : r), 1
+							}
+							if (
+								((l = 0),
+								(r = '' === r ? '.' : r + ':'),
+								Array.isArray(t))
+							) {
+								for (var i = 0; i < t.length; i++) {
+									var a = r + q((f = t[i]), i)
+									l += e(f, a, n, o)
+								}
+							} else if (
+								(null === t || 'object' !== typeof t
+									? (a = null)
+									: (a =
+											'function' ===
+											typeof (a =
+												(m && t[m]) || t['@@iterator'])
+												? a
+												: null),
+								'function' === typeof a)
+							) {
+								for (
+									t = a.call(t), i = 0;
+									!(f = t.next()).done;
+
+								) {
+									l += e(
+										(f = f.value),
+										(a = r + q(f, i++)),
+										n,
+										o
+									)
+								}
+							} else if ('object' === f) {
+								throw ((n = String(t)),
+								b(
+									Error(31),
+									'[object Object]' === n
+										? 'object with keys {' +
+												Object.keys(t).join(', ') +
+												'}'
+										: n,
+									''
+								))
+							}
+							return l
+					  })(e, '', t, r)
+			}
+			function q(e, t) {
+				return 'object' === typeof e && null !== e && null != e.key
+					? (function(e) {
+							const t = { '=': '=0', ':': '=2' }
+							return (
+								'$' +
+								String(e).replace(/[=:]/g, function(e) {
+									return t[e]
+								})
+							)
+					  })(e.key)
+					: t.toString(36)
+			}
+			function L(e, t) {
+				e.func.call(e.context, t, e.count++)
+			}
+			function U(e, t, r) {
+				const n = e.result,
+					o = e.keyPrefix
+				;(e = e.func.call(e.context, t, e.count++)),
+					Array.isArray(e)
+						? T(e, n, r, function(e) {
+								return e
+						  })
+						: null != e &&
+						  (R(e) &&
+								(e = (function(e, t) {
+									return {
+										$$typeof: u,
+										type: e.type,
+										key: t,
+										ref: e.ref,
+										props: e.props,
+										_owner: e._owner
+									}
+								})(
+									e,
+									o +
+										(!e.key || (t && t.key === e.key)
+											? ''
+											: String(e.key).replace(V, '$&/') +
+											  '/') +
+										r
+								)),
+						  n.push(e))
+			}
+			function T(e, t, r, n, o) {
+				let u = ''
+				null != r && (u = String(r).replace(V, '$&/') + '/'),
+					I(e, U, (t = z(t, u, n, o))),
+					A(t)
+			}
+			function F() {
+				const e = x.current
+				if (null === e) {
+					throw b(Error(321))
+				}
+				return e
+			}
+			const H = {
+					Children: {
+						map(e, t, r) {
+							if (null == e) {
+								return e
+							}
+							const n = []
+							return T(e, n, null, t, r), n
+						},
+						forEach(e, t, r) {
+							if (null == e) {
+								return e
+							}
+							I(e, L, (t = z(null, null, t, r))), A(t)
+						},
+						count(e) {
+							return I(
+								e,
+								function() {
+									return null
+								},
+								null
+							)
+						},
+						toArray(e) {
+							const t = []
+							return (
+								T(e, t, null, function(e) {
+									return e
+								}),
+								t
+							)
+						},
+						only(e) {
+							if (!R(e)) {
+								throw b(Error(143))
+							}
+							return e
+						}
+					},
+					createRef() {
+						return { current: null }
+					},
+					Component: j,
+					PureComponent: w,
+					createContext(e, t) {
+						return (
+							void 0 === t && (t = null),
+							((e = {
+								$$typeof: s,
+								_calculateChangedBits: t,
+								_currentValue: e,
+								_currentValue2: e,
+								_threadCount: 0,
+								Provider: null,
+								Consumer: null
+							}).Provider = { $$typeof: a, _context: e }),
+							(e.Consumer = e)
+						)
+					},
+					forwardRef(e) {
+						return { $$typeof: p, render: e }
+					},
+					lazy(e) {
+						return {
+							$$typeof: h,
+							_ctor: e,
+							_status: -1,
+							_result: null
+						}
+					},
+					memo(e, t) {
+						return {
+							$$typeof: v,
+							type: e,
+							compare: void 0 === t ? null : t
+						}
+					},
+					useCallback(e, t) {
+						return F().useCallback(e, t)
+					},
+					useContext(e, t) {
+						return F().useContext(e, t)
+					},
+					useEffect(e, t) {
+						return F().useEffect(e, t)
+					},
+					useImperativeHandle(e, t, r) {
+						return F().useImperativeHandle(e, t, r)
+					},
+					useDebugValue() {},
+					useLayoutEffect(e, t) {
+						return F().useLayoutEffect(e, t)
+					},
+					useMemo(e, t) {
+						return F().useMemo(e, t)
+					},
+					useReducer(e, t, r) {
+						return F().useReducer(e, t, r)
+					},
+					useRef(e) {
+						return F().useRef(e)
+					},
+					useState(e) {
+						return F().useState(e)
+					},
+					Fragment: f,
+					Profiler: i,
+					StrictMode: l,
+					Suspense: y,
+					unstable_SuspenseList: d,
+					createElement: $,
+					cloneElement(e, t, r) {
+						if (null == e) {
+							throw b(Error(267), e)
+						}
+						let o = void 0,
+							c = n({}, e.props),
+							f = e.key,
+							l = e.ref,
+							i = e._owner
+						if (null != t) {
+							void 0 !== t.ref && ((l = t.ref), (i = E.current)),
+								void 0 !== t.key && (f = String(t.key))
+							var a = void 0
+							for (o in (e.type &&
+								e.type.defaultProps &&
+								(a = e.type.defaultProps),
+							t)) {
+								P.call(t, o) &&
+									!C.hasOwnProperty(o) &&
+									(c[o] =
+										void 0 === t[o] && void 0 !== a
+											? a[o]
+											: t[o])
+							}
+						}
+						if (1 === (o = arguments.length - 2)) {
+							c.children = r
+						} else if (1 < o) {
+							a = Array(o)
+							for (let s = 0; s < o; s++) {
+								a[s] = arguments[s + 2]
+							}
+							c.children = a
+						}
+						return {
+							$$typeof: u,
+							type: e.type,
+							key: f,
+							ref: l,
+							props: c,
+							_owner: i
+						}
+					},
+					createFactory(e) {
+						const t = $.bind(null, e)
+						return (t.type = e), t
+					},
+					isValidElement: R,
+					version: '16.9.0',
+					unstable_withSuspenseConfig(e, t) {
+						const r = k.suspense
+						k.suspense = void 0 === t ? null : t
+						try {
+							e()
+						} finally {
+							k.suspense = r
+						}
+					},
+					__SECRET_INTERNALS_DO_NOT_USE_OR_YOU_WILL_BE_FIRED: {
+						ReactCurrentDispatcher: x,
+						ReactCurrentBatchConfig: k,
+						ReactCurrentOwner: E,
+						IsSomeRendererActing: { current: !1 },
+						assign: n
+					}
+				},
+				N = { default: H },
+				B = (N && H) || N
+			e.exports = B.default || B
+		},
+		function(e, t, r) {
+			'use strict'
+			/*
+object-assign
+(c) Sindre Sorhus
+@license MIT
+*/ const n =
+					Object.getOwnPropertySymbols,
+				o = Object.prototype.hasOwnProperty,
+				u = Object.prototype.propertyIsEnumerable
+			function c(e) {
+				if (null == e) {
+					throw new TypeError(
+						'Object.assign cannot be called with null or undefined'
+					)
+				}
+				return Object(e)
+			}
+			e.exports = (function() {
+				try {
+					if (!Object.assign) {
+						return !1
+					}
+					const e = new String('abc')
+					if (
+						((e[5] = 'de'),
+						'5' === Object.getOwnPropertyNames(e)[0])
+					) {
+						return !1
+					}
+					for (var t = {}, r = 0; r < 10; r++) {
+						t['_' + String.fromCharCode(r)] = r
+					}
+					if (
+						'0123456789' !==
+						Object.getOwnPropertyNames(t)
+							.map(function(e) {
+								return t[e]
+							})
+							.join('')
+					) {
+						return !1
+					}
+					const n = {}
+					return (
+						'abcdefghijklmnopqrst'.split('').forEach(function(e) {
+							n[e] = e
+						}),
+						'abcdefghijklmnopqrst' ===
+							Object.keys(Object.assign({}, n)).join('')
+					)
+				} catch (e) {
+					return !1
+				}
+			})()
+				? Object.assign
+				: function(e, t) {
+						for (
+							var r, f, l = c(e), i = 1;
+							i < arguments.length;
+							i++
+						) {
+							for (const a in (r = Object(arguments[i]))) {
+								o.call(r, a) && (l[a] = r[a])
+							}
+							if (n) {
+								f = n(r)
+								for (let s = 0; s < f.length; s++) {
+									u.call(r, f[s]) && (l[f[s]] = r[f[s]])
+								}
+							}
+						}
+						return l
+				  }
+		},
+		function(e, t, r) {
+			'use strict'
+			r.r(t)
+			const n = r(0),
+				o = r.n(n)
+			const u = () => o.a.createElement('div', null, 'Overlay')
+			const c = e =>
+				o.a.createElement(
+					'svg',
+					Object.assign(
+						{
+							width: '148',
+							height: '37',
+							viewBox: '0 0 148 37',
+							fill: 'none',
+							xmlns: 'http://www.w3.org/2000/svg'
+						},
+						e
+					),
+					o.a.createElement('path', {
+						d:
+							'M24.235 18.654c2.744-1.176 4.655-3.283 4.655-7.154 0-7.84-6.909-7.84-13.72-7.84H.96v1.568c1.764 0 2.45 1.176 2.45 4.312v20.58c0 3.136-.686 4.312-2.45 4.312V36h15.19c6.125 0 14.014.049 14.014-8.771 0-4.949-2.646-7.399-5.929-8.575zM10.172 6.208h4.312c5.194 0 7.644.686 7.644 5.635s-2.45 5.635-7.644 5.635h-4.312V6.208zm5.586 27.244h-5.586V20.026h5.586c5.194 0 7.644 1.764 7.644 6.713s-2.45 6.713-7.644 6.713zM37.931 9.148c2.205 0 3.577-1.274 3.577-3.038 0-1.617-1.372-2.94-3.577-2.94-2.254 0-3.577 1.323-3.577 2.94 0 1.764 1.323 3.038 3.577 3.038zm3.87 6.615c0-4.018-2.008-4.263-5.977-4.263H32.59v1.568c1.764 0 2.45 1.176 2.45 4.312v12.74c0 3.136-.686 4.312-2.45 4.312V36h11.662v-1.568c-1.764 0-2.45-1.176-2.45-4.312V15.763zM73.706 30.12v-9.751c0-6.37-3.479-9.359-8.477-9.359-4.949 0-7.79 2.94-8.82 7.546h-.49v-2.989c0-3.822-2.156-4.067-5.978-4.067h-3.234v1.568c1.764 0 2.45 1.176 2.45 4.312v12.74c0 3.136-.686 4.312-2.45 4.312V36h11.662v-1.568c-1.764 0-2.45-1.176-2.45-4.312v-2.94c0-6.713 2.254-13.622 7.252-13.622 2.695 0 3.773 1.862 3.773 5.145V30.12c0 3.136-.686 4.312-2.45 4.312V36h11.662v-1.568c-1.764 0-2.45-1.176-2.45-4.312zm3.817-6.37c0 8.33 5.243 12.74 11.515 12.74 3.97 0 7.4-1.862 9.163-5.096h.49v.931c0 3.43 2.304 3.675 5.979 3.675h3.234v-1.568c-1.764 0-2.45-1.176-2.45-4.312V5.032c0-4.018-2.156-4.312-5.978-4.312H96.24v1.568c1.763 0 2.45 1.176 2.45 4.312v9.506h-.49c-1.764-3.234-5.194-5.096-9.163-5.096-6.272 0-11.516 4.41-11.516 12.74zm7.252 0c0-6.125 1.96-10.192 6.567-10.192 4.606 0 6.86 4.067 6.86 10.192s-2.254 10.192-6.86 10.192-6.566-4.067-6.566-10.192zM119.61 5.032c0-4.018-2.156-4.312-5.978-4.312h-3.234v1.568c1.764 0 2.45 1.176 2.45 4.312v23.52c0 3.136-.686 4.312-2.45 4.312V36h11.662v-1.568c-1.764 0-2.45-1.176-2.45-4.312V5.032zm21.387 23.03c.637 4.508-1.127 5.88-4.165 5.88-4.361 0-6.321-4.116-6.37-9.065 6.566 1.666 17.444.833 17.444-6.664 0-4.606-4.312-7.203-10.388-7.203-8.232 0-13.818 5.047-13.818 13.377 0 6.86 3.822 12.103 13.328 12.103 7.35 0 11.221-4.459 10.731-9.702l-6.762 1.274zm-4.116-14.504c2.842 0 4.263 1.617 4.263 4.508 0 4.067-3.626 5.831-10.584 4.165.392-5.341 2.156-8.673 6.321-8.673z',
+						fill: '#000'
+					})
+				)
+			r.d(t, 'Overlay', function() {
+				return u
+			}),
+				r.d(t, 'Logo', function() {
+					return c
+				})
+		}
+	])
+})
