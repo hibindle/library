f2cd44e97a0f12401b2da09445cd0d3784dca56b
Isaac Martin <isaac@weareplayground.co>
2019-09-02 09:15:38 -0400

    init

---
+++ variables/variables.ts
@@ -0,0 +1,19 @@
+module.exports = {
+  // Whitespace
+  whitespaceSmallest: '0.2rem',
+	whitespaceSmall: '0.4rem',
+	whitespaceSmallLess: '0.8rem',
+	whitespaceSmallBase: '1.2rem',
+	whitespaceSmallMore: '1.6rem',
+	whitespaceLargeLess: '2.0rem',
+	whitespaceLargeBase: '2.4rem',
+	whitespaceLargeMoreBase: '2.8rem',
+	whitespaceLargeMore: '3.2rem',
+  whitespaceExtraLargeLess: '3.6rem',
+  
+
+  	// Border radii
+	borderRadiusSmall: '2px',
+	borderRadiusBase: '4px',
+	borderRadiusLarge: '6px',
+}
